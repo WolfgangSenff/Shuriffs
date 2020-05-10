5bb9630b14c634dfc2a09b6189e12e7def16d0c9
Kyle Szklenski <kyle.szklenski@gmail.com>
2020-05-10 06:25:00 -0400

    Major furniture update

---
+++ assets/resources/SheriffResource.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name SheriffResource
+
+export (String) var NickName
+export (String) var FullName
+export (String, MULTILINE) var CharacterDescription
+export (Resource) var AttackListResource
+export (Texture) var Portrait
+
+export (PackedScene) var SheriffScene
+export (Resource) var DeputyListResource
